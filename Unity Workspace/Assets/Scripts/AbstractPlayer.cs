5a33428d7375fb05fd628f96ad0bcd4d58e15dc7
lordkevinthebold <kevinbourassahoule@gmail.com>
2015-04-05 16:15:27 -0400

    Initial push

---
+++ Unity Workspace/Assets/Scripts/AbstractPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractPlayer : MonoBehaviour 
+{
+	public GameObject Bullet;
+	public float MAX_HEALTH;
+	public int Speed;
+	
+	protected int magazine;
+	protected float health;
+	
+	protected void Shoot()
+	{
+		GameObject.Instantiate(Bullet,
+							   transform.position,
+							   transform.rotation);
+	}
+	
+	protected abstract void Move(Vector3 direction);
+}
---
+++ Unity Workspace/Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayer : AbstractPlayer
+{
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Handle orientation
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		if (Physics.Raycast(ray, out hit))
+		{
+			transform.LookAt(hit.point);
+		}
+			
+		// Handle shooting
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+		
+		// Handle movement
+		Vector3 direction = Vector3.zero;
+		
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			direction += Vector3.up;
+		}
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			direction += Vector3.left;
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			direction += Vector3.down;
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			direction += Vector3.right;
+		}
+		
+		Move(direction);
+	}
+	
+	override protected void Move(Vector3 direction)
+	{
+		transform.position += direction.normalized * Speed;
+	}
+}
