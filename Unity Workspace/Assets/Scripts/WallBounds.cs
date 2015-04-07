ab4734d653bdd2de66679d4f8b48ee6e788aee94
joshromoff <joshua.romoff@mail.mcgill.ca>
2015-04-06 23:08:17 -0400

    fixed bounding rays

--- Unity Workspace/Assets/Scripts/WallBounds.cs
+++ Unity Workspace/Assets/Scripts/WallBounds.cs
@@ -14,7 +14,17 @@ public class WallBounds : MonoBehaviour {
 		botRight = new Vector2 (playerExtents.x + transform.position.x, -playerExtents.y + transform.position.y);
 		topLeft = new Vector2 (-playerExtents.x + transform.position.x, playerExtents.y + transform.position.y);
 		topRight = new Vector2 (playerExtents.x + transform.position.x, playerExtents.y + transform.position.y);
+
+		Quaternion angle = Quaternion.Euler (transform.eulerAngles);
+		botLeft = RotatePoint2d (botLeft, angle);
+		botRight = RotatePoint2d (botRight, angle);
+		topLeft = RotatePoint2d (topLeft, angle);
+		topRight = RotatePoint2d (topRight, angle);
+
 	}
 	
-
+	private Vector2 RotatePoint2d(Vector2 oldPoint, Quaternion angle)   
+	{
+		return (Vector2) (angle * ((Vector3)oldPoint - transform.position) + transform.position);    
+	}
 }
