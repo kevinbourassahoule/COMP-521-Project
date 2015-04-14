19dc61ce27cb678a9eaf11e14be9e6f504b8db9e
joshromoff <joshua.romoff@mail.mcgill.ca>
2015-04-14 15:13:54 -0400

    fixed covers... too bad its past the demo lol

--- Unity Workspace/Assets/Scripts/AIPlayer.cs
+++ Unity Workspace/Assets/Scripts/AIPlayer.cs
@@ -183,11 +183,11 @@ public class AIPlayer : AbstractPlayer
 			Vector2 coverForce  = nearestCover - (Vector2)transform.position;
 			//Debug.Log(coverForce);
 			movementForces +=  coverForce;
-			lookAtForces   -=  coverForce;
+			lookAtForces   -=  coverForce.normalized/Vector2.Distance(nearestCover,(Vector2)transform.position);
 		}
 
 
-		// Get nearest cover point from nearest wall and generate a force from its position
+		/*// Get nearest cover point from nearest wall and generate a force from its position
 		if (Vector2.Distance(nearestWall.coverLeftPoint, transform.position) < 
 		    Vector2.Distance(nearestWall.coverRightPoint, transform.position)) 
 		{
@@ -200,7 +200,7 @@ public class AIPlayer : AbstractPlayer
 		{
 			movementForces += nearbyCoverForce;
 			lookAtForces   += nearbyCoverForce;
-		}
+		}*/
 		
 		// Accumulate forces from walls
 		if (nearbyWallCount > 0) 
